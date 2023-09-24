40b995bbfa5b04201f5b05392117f20230f9d480
newbee1939 <hideto3970@gmail.com>
2023-09-24 11:10:23 +0900

    fix

--- app/sample.spec.ts
+++ app/sample.spec.ts
@@ -1,31 +1,23 @@
 import { getResult } from "./sample";
 
 describe("getResult", () => {
-  describe("3の倍数でも5の倍数でもあるとき", () => {
-    test("0を返す", () => {
-      const result = getResult(15);
-      expect(result).toBe("0");
-    });
+  test("3の倍数でも5の倍数でもあるとき0を返す", () => {
+    const result = getResult(15);
+    expect(result).toBe("0");
   });
 
-  describe("5の倍数のとき", () => {
-    test("1を返す", () => {
-      const result = getResult(25);
-      expect(result).toBe("1");
-    });
+  test("5の倍数のとき1を返す", () => {
+    const result = getResult(25);
+    expect(result).toBe("1");
   });
 
-  describe("3の倍数のとき", () => {
-    test("2を返す", () => {
-      const result = getResult(48);
-      expect(result).toBe("2");
-    });
+  test("3の倍数のとき2を返す", () => {
+    const result = getResult(48);
+    expect(result).toBe("2");
   });
 
-  describe("それ以外のとき", () => {
-    test("3を返す", () => {
-      const result = getResult(112);
-      expect(result).toBe("3");
-    });
+  test("それ以外のとき3を返す", () => {
+    const result = getResult(112);
+    expect(result).toBe("3");
   });
 });
