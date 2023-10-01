7a8a557324c166629c74bd2b73dfc5f66fb40c21
newbee1939 <hideto3970@gmail.com>
2023-10-01 17:12:46 +0900

    update

--- app/sample.ts
+++ app/sample.ts
@@ -5,3 +5,8 @@ export const groupBy = <T>(values: T[], key: keyof T) => {
     return prev;
   }, {} as Record<string, T[]>);
 };
+
+// https://www.engilaboo.com/typescript-utility-types/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/groupBy
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Map/groupBy
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/groupBy
